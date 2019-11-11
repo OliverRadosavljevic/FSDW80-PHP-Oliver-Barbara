d095ccee0fc245065d7a25b02eddea8f35c487a8
Oliver <oliver.radosavljevic@hotmail.com>
2019-11-11 15:09:27 +0100

    finished exercises

---
+++ Oliver/classwork.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Classwork</title>
+	<link rel="stylesheet" href="">
+	<style type="text/css" media="screen">
+		
+		/* #ex1{
+			left: 0;
+		    line-height: 200px;
+		    margin-top: -100px;
+		    position: absolute;
+		    text-align: center;
+		    top: 50%;
+		    width: 100%;
+		} */
+	</style>
+</head>
+<body>
+	<?php
+	/*Exercise 1*/ 
+	// $myName = "Oliver Radosavjevic";
+	//  echo "<h1 id='ex1' >$myName</h1>";
+
+	//Exercise 2
+	// $date = date("D");
+	// if($date == "Mon"){
+	// 	echo "Happy Monday!";
+	// }else{
+	// 	echo "Have a nice day!";
+	// }
+
+	//Exercise 3
+	// $date = date("D");
+	// if($date == "Mon"){
+	// 	echo "Today is Monday";
+	// }elseif($date == "Tue"){
+	//  	echo "Today is Tuesday";
+	// }elseif($date == "Wed"){
+	// 	echo "Today is Wednesday";
+	// }elseif($date == "Thu"){
+	// 	echo "Today is Thursday";
+	// }elseif($date == "Fri"){
+	// 	echo "Today is Friday";
+	// }elseif($date == "Sat"){
+	// 	echo "Today is Saturday";
+	// }elseif($date == "Sun"){
+	// 	echo "Today is Sunday";
+	// }
+	
+
+
+
+	//Exercise 4
+	// echo "For-Loop <br>";
+	// for($i = 0; $i <50; $i++){
+	// 	echo "Oliver Radosavljevic <br>";
+	// }
+
+	// echo "<br><br><br><br>";
+
+	// $a = 0;
+	// echo "While-Loop <br>";
+	// while($a < 50){
+	//  echo "Oliver Radosavljevic <br>";
+	//  $a++;
+	// }
+
+	// echo "<br><br><br><br>";
+
+	// $b = 0;
+	// echo "DoWhile-Loop <br>";
+	// do{
+	//  echo "Oliver Radosavljevic <br>";
+	//  $b++;
+	// }while($b < 50)
+
+
+
+	//Exercise 5
+	// $arr = array(1,2,3,4,5,6,7,8,9,10);
+	// foreach ($arr as $value) {
+	// 	echo "$value <br>";
+	// }
+
+
+
+
+	//Exercise 6
+	// $chars = array(
+	// "Link" => array(
+	// 	"weight" => 63,
+	// 	"height" => 176,
+	// 	"color"  => "green"
+	// ),
+	// "Deku" => array(
+	// 	"weight" => 62,
+	// 	"height" => 166,
+	// 	"color"  => "green"
+	// ),
+	// "Soma" => array(
+	// 	"weight" => 57,
+	// 	"height" => 173,
+	// 	"color"  => "red"
+	// )
+	// );
+
+	// if($chars["Link"]["weight"] > $chars["Deku"]["weight"] && $chars["Link"]["weight"] > $chars["Soma"]["weight"]){
+	// 	echo "Link is the fattest";
+	// }elseif($chars["Link"]["weight"] < $chars["Deku"]["weight"] && $chars["Deku"]["weight"] > $chars["Soma"]["weight"]){
+	// 	echo "Deku is the fattest";
+	// }elseif($chars["Link"]["weight"] < $chars["Soma"]["weight"] && $chars["Deku"]["weight"] < $chars["Soma"]["weight"]){
+	// 	echo "Soma is the fattest";
+	// }
+	?>
+</body>
+</html>
